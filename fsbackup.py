45a43f9d3c14be8eeb18b7d981ce382cda763209
Ramon Moraes <ramon@vyscond.io>
2019-08-28 22:39:44 -0400

    Refactored the workflow to back files and add support for CLI

--- fsbackup.py
+++ fsbackup.py
@@ -2,60 +2,57 @@ import os
 import yaml
 import boto3
 
-class FSBackup(object):
+class FsBackup(object):
 
-	def __init__(self, config_path):
-		self.config = yaml.safe_load(open('config.yaml', 'r'))
+    def __init__(self, config_path):
+        self.config = yaml.safe_load(open('config.yaml', 'r'))
+        self.dry_run = False
 
-	def get_s3_client(self):
-		session = boto3.Session(profile_name=self.config['aws']['profile'])
-		s3 = session.client('s3')
-		return s3
+    def get_s3_client(self):
+        session = boto3.Session(profile_name=self.config['aws']['profile'])
+        s3 = session.client('s3')
+        return s3
 
-	def create_bucket(self):
-		s3 = self.get_s3_client()
-		s3.create_bucket(
-			Bucket=self.config['aws']['bucket'],
-			)
+    def create_bucket(self):
+        s3 = self.get_s3_client()
+        s3.create_bucket(
+            Bucket=self.config['aws']['bucket'],
+            )
 
-	def push(self, source, destination):
-		s3 = self.get_s3_client()
-		bucket = self.config['aws']['bucket']
-		source = os.path.expanduser(source)
-		if os.path.isfile(source):
-			file_path_s3 = destination
-			print(f"Uploading {source} to s3://{bucket}/{file_path_s3}")
-			# s3.put_object(
-			# 	Bucket=bucket,
-			# 	Body=open(source, 'rb').read(),
-			# 	Key=file_path_s3,
-			# 	)
-		else:
-			for root, dirs, files in os.walk(source):
-				for file in files:
-					file_path = os.path.expanduser(
-						os.path.join(
-							root, file))
-					file_path_s3 = os.path.join(
-						destination, file)
-					print(f"Uploading {file_path} to s3://{bucket}/{file_path_s3}")
-					s3.put_object(
-						Bucket=bucket,
-						Body=open(file_path, 'rb').read(),
-						Key=file_path_s3,
-						)
+    def delete_bucket(self):
+        s3 = self.get_s3_client()
+        resp = s3.delete_bucket(
+            Bucket='string'
+        )
 
-	def sync(self, from_localtion, to_location):
-		if from_localtion == 'local':
-			for entry in self.config['backups'][to_location]:
-				print(f"Sync files from {from_localtion} to {to_location}")
-				for entry in self.config['backups'][to_location]:
-					self.push(
-						entry['source'], 
-						os.path.join(to_location, entry['destination']),
-						)
+    def backup(self, device_name):
+        s3 = self.get_s3_client()
+        bucket = self.config['aws']['bucket']
+        files_for_upload = []
+        for entry in self.config['backups'][device_name]:
+            entry = os.path.expanduser(entry)
+            if os.path.isfile(entry):
+                file_path_s3 = device_name + entry
+                files_for_upload.append((entry, file_path_s3))
+            else:
+                for root, dirs, files in os.walk(entry):
+                    for file in files:
+                        file_path = os.path.expanduser(os.path.join(root, file))
+                        file_path_s3 = device_name + file_path
+                        files_for_upload.append((file_path, file_path_s3))
+        
+        for entry in files_for_upload:
+            if not self.dry_run:
+                print(f"Uploading {entry[0]} to s3://{entry[1]}")
+                s3.put_object(
+                    Bucket=bucket,
+                    Body=open(entry[0], 'rb').read(),
+                    Key=entry[1],
+                    )
+
 
 if __name__ == '__main__':
-	fsbackup = FSBackup('config.yaml')
-	fsbackup.create_bucket()
-	fsbackup.sync('local', 'fedora')
+    fsbackup = FsBackup('config.yaml')
+    # fsbackup.delete_bucket()
+    fsbackup.create_bucket()
+    fsbackup.backup('fedora')
