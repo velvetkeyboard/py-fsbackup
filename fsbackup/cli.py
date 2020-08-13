de9a47555b043541e00fea572c536f7aab522e45
Ramon Moraes <ramonmoraes8080@gmail.com>
2020-08-13 20:04:41 -0300

    Update CLI - adjusting defaults functions

--- fsbackup/cli.py
+++ fsbackup/cli.py
@@ -5,11 +5,12 @@ from fsbackup.lib import FsBackup
 
 
 FSBACKUP_YAML = '~/.fsbackup.yaml'
+FSBACKUP_JSON = '~/.fsbackup.json'
 
 
 def get_backend_by_name(name):
     if name == 'google_drive':
-        from fsbackup.backends import GoogleDriveBackend
+        from fsbackup_googledrive.backends import GoogleDriveBackend
         ret = GoogleDriveBackend
     elif name == 'local':
         from fsbackup.backends import LocalBackend
@@ -17,59 +18,45 @@ def get_backend_by_name(name):
     else:
         from fsbackup.backends import LocalBackend
         ret = LocalBackend
-    return ret 
+    return ret
 
 
-class BaseAction(argparse.Action):
-
-    def __init__(self, **kwargs):
-        super(BaseAction, self).__init__(**kwargs)
-
-    def __call__(self, parser, namespace, values, option_string=None):
-        self.cmd(namespace)
-
-    def cmd(self, namespace):
-        raise NotImplementedError('.cmd() not defined')
-
-    def get_cfg_path(self, namespace):
-        return os.path.expanduser(
-            namespace.config or FSBACKUP_YAML)
+def get_cfg_path(config):
+    return os.path.expanduser(
+            config or FSBACKUP_YAML)
 
 
-class Upload(BaseAction):
-    def cmd(self, namespace):
-        cfg = self.get_cfg_path(namespace)
-        backend = get_backend_by_name(namespace.backend)(cfg)
-        password = None
-        if namespace.encryption:
-            password = getpass.getpass()
-        FsBackup(cfg).backup(
-            namespace.schema,
-            backend,
-            password=password,
-            encryption_engine=namespace.encryption,
-            )
+def upload(args):
+    cfg = get_cfg_path(args.config)
+    backend = get_backend_by_name(args.backend)(cfg)
+    password = None
+    if args.encryption:
+        password = getpass.getpass()
+    FsBackup(cfg).backup(
+        args.schema,
+        backend,
+        password=password,
+        encryption_engine=args.encryption,
+        )
 
 
-class Listing(BaseAction):
-    def cmd(self, namespace):
-        cfg = self.get_cfg_path(namespace)
-        backend = get_backend_by_name(namespace.backend)(cfg)
+def listing(args):
+    cfg = get_cfg_path(args.config)
+    backend = get_backend_by_name(args.backend)(cfg)
+    for i, path in enumerate(backend.list()):
+        print(f'[{i}] {path}')
+
+
+def download(args):
+    cfg = get_cfg_path(args.config)
+    backend = get_backend_by_name(args.backend)(cfg)
+    if not args.number:
         for i, path in enumerate(backend.list()):
             print(f'[{i}] {path}')
-
-
-class Download(BaseAction):
-    def cmd(self, namespace):
-        cfg = self.get_cfg_path(namespace)
-        backend = get_backend_by_name(namespace.backend)(cfg)
-        if not namespace.number:
-            for i, path in enumerate(backend.list()):
-                print(f'[{i}] {path}')
-            idx = int(input('Number: '))
-        else:
-            idx = int(namespace.number)
-        backend.download(idx)
+        idx = int(input('Number: '))
+    else:
+        idx = int(args.number)
+    backend.download(idx)
 
 
 def main():
@@ -79,39 +66,42 @@ def main():
             description='backup your files'
             )
     sparser = parser.add_subparsers(help='actions help')
+
     # ------------------------------------------------------------------[upload]
-    upload = sparser.add_parser('upload', help='upload files to a backend')
-    upload.add_argument(
-        '-c', '--config', type=str, help='config.yml file path')
-    upload.add_argument(
+    upload_parser = sparser.add_parser(
+            'upload', help='upload_parser files to a backend')
+    upload_parser.add_argument(
+        '-c', '--config', type=str, default=FSBACKUP_JSON, help='config.yml file path')
+    upload_parser.add_argument(
         '-s', '--schema', type=str, help="config file's schema to use")
-    upload.add_argument(
-        '-b', '--backend', type=str, help='service you want to use to upload')
-    upload.add_argument(
+    upload_parser.add_argument(
+        '-b', '--backend', type=str, help='service you want to use to upload_parser')
+    upload_parser.add_argument(
         '-e', '--encryption', action='store_true', help='encryption engine')
-    upload.add_argument(
-        'run', nargs=0, action=Upload, help=argparse.SUPPRESS)
+    upload_parser.set_defaults(func=upload)
+
     # -----------------------------------------------------------------[listing]
-    listing = sparser.add_parser(
+    list_parser = sparser.add_parser(
         'list', help='download backup files from a backend')
-    listing.add_argument(
-        '-c', '--config', type=str, help='config.yml file path')
-    listing.add_argument(
+    list_parser.add_argument(
+        '-c', '--config', type=str, default=FSBACKUP_JSON, help='config.yml file path')
+    list_parser.add_argument(
         '-b', '--backend', type=str, help='service you want to use to upload')
-    listing.add_argument(
-        'run', nargs=0, action=Listing, help=argparse.SUPPRESS)
+    list_parser.set_defaults(func=listing)
+
     # ----------------------------------------------------------------[Download]
-    download = sparser.add_parser(
-        'download', help='download backup files from a backend')
-    download.add_argument(
-        '-c', '--config', type=str, help='config.yml file path')
-    download.add_argument(
+    download_parser = sparser.add_parser(
+        'download', help='download_parser backup files from a backend')
+    download_parser.add_argument(
+        '-c', '--config', type=str, default=FSBACKUP_JSON, help='config.yml file path')
+    download_parser.add_argument(
         '-b', '--backend', type=str, help='service you want to use to upload')
-    download.add_argument(
+    download_parser.add_argument(
         '-n', '--number', type=str, help='number index')
-    download.add_argument(
-        'run', nargs=0, action=Download, help=argparse.SUPPRESS)
-    parser.parse_args()
+    download_parser.set_defaults(func=download)
+
+    args = parser.parse_args()
+    args.func(args)
 
 
 if __name__ == '__main__':
